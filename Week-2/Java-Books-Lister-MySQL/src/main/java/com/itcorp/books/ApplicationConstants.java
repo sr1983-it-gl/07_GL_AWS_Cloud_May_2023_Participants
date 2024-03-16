d108271606cc5201c126fdd29304b3ba9ae712f9
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-03-16 18:28:37 +0530

    Files

--- Week-2/Java-Books-Lister-MySQL/src/main/java/com/itcorp/books/ApplicationConstants.java
+++ Week-2/Java-Books-Lister-MySQL/src/main/java/com/itcorp/books/ApplicationConstants.java
@@ -8,16 +8,19 @@ public class ApplicationConstants {
 	public static String SERVER_NAME = "localhost";
 	
 	public static int DB_PORT = 4444;
-
-	public static String DB_NAME = "books_db";
 	
+//	public static int DB_PORT = 3306;
+
+	public static String DB_NAME = "books_db";	
 	
 	public static String CONNECTION_URL
 		= String.format("jdbc:mysql://%s:%d/%s", 
 				SERVER_NAME, DB_PORT, DB_NAME);
 
-	public static String USERNAME = "book_user";
+//	public static String USERNAME = "book_user";
+	public static String USERNAME = "root";
 	
-	public static String PASSWORD = "password_123";
+//	public static String PASSWORD = "password_123";
+	public static String PASSWORD = "mypassword_11";
 
 }
